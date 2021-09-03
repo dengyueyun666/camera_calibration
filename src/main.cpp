88a6ba81fa27aef452a6a7abc1bee8a86ba99eea
dengyueyun <dengyueyun666@sina.com>
2021-09-03 11:52:16 +0800

    Add calib_settings.yaml and .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,20 @@
 
 #include "util.h"
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        cout << "Usage :" << endl
+             << "./main <image_path>" << endl;
+        return -1;
+    }
+
+    string config_file = argv[1];
+
+    cout << config_file << endl;
+
     return 0;
 }
